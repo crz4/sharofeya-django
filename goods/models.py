837fa91df2c58ac8329d43dfb0d5647f39fa85e2
filipp <kartashovfilipp@MacBook-Air-filipp.local>
2026-01-22 22:09:07 +0300

    Создал админку, все подключил

--- goods/models.py
+++ goods/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=150, unique=True, verbose_name='Название')
+    slug = models.SlugField(max_length=200, unique=True, blank=True, null=True, verbose_name='URL')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        db_table = 'category'
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+
+class Product(models.Model):
+    title = models.CharField(max_length=200, unique=True, verbose_name='Название товара')
+    slug = models.SlugField(max_length=200, unique=True, blank=True, null=True, verbose_name='URL')
+    price = models.DecimalField(verbose_name='Цена',default=0.00, max_digits=7, decimal_places=2 )
+    image = models.ImageField(upload_to='products/', verbose_name='Фото', blank=True, null=True)
+    description = models.TextField(verbose_name='Описание', blank=True, null=True)
+    is_active = models.BooleanField(default=True, verbose_name='Активен')
+    category = models.ForeignKey(verbose_name='Категория', to=Category, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Товар'
+        verbose_name_plural = 'Товары'
